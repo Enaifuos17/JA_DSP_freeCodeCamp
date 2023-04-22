07f335a19d2945caf25c5a0cad351aa4c7c0ee3e
Enaifuos17 <soufianeelfaquir2001@gmail.com>
2023-04-22 02:51:15 +0000

    3/5 - Caesars Cipher

--- script.js
+++ script.js
@@ -124,3 +124,62 @@ console.log("----------------------");
 // * ------------------------------------------------------------
 console.log("----------------------");
 // * ------------------------------------------------------------
+
+/*
+
+? Caesars Cipher
+
+One of the simplest and most widely known ciphers is:
+a Caesar cipher, 
+also known as a shift cipher. 
+
+In a shift cipher 
+the meanings of the letters are shifted by some set amount.
+
+A common modern use is the ROT13 cipher, 
+where the values of the letters are shifted by 13 places. 
+Thus A ↔ N, B ↔ O and so on.
+
+Write a function which takes a ROT13 encoded string as input 
+and returns a decoded string.
+
+All letters will be uppercase. 
+Do not transform any non-alphabetic character 
+(i.e. spaces, punctuation), but do pass them on.
+
+*/
+
+function rot13(str) {
+  let answer = [];
+  let alphbtReg = /[A-Z]/;
+  let first13 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let second13 = "NOPQRSTUVWXYZABCDEFGHIJKLM";
+  // mainLoop
+  for (let i = 0; i < str.length; i++) {
+    // Check each letter with the str[i]
+    for (let j = 0; j < first13.length; j++) {
+      if (str[i] == first13[j]) {
+        answer.push(second13[j]);
+        break;
+      }
+    }
+    // if str[i] is not a letter
+    if (!alphbtReg.test(str[i])) {
+      answer.push(str[i]);
+    }
+  }
+  return answer.join("");
+}
+
+console.log(rot13("SERR PBQR PNZC")); // FREE CODE CAMP
+console.log(rot13("SERR CVMMN!")); // FREE PIZZA!
+console.log(rot13("SERR YBIR?")); // FREE LOVE?
+console.log(rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT."));
+// THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
+
+// * ------------------------------------------------------------
+console.log("----------------------");
+// * ------------------------------------------------------------
+// * ------------------------------------------------------------
+console.log("----------------------");
+// * ------------------------------------------------------------
